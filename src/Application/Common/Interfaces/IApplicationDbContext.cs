1ae73485b5e74c32fd2462f5afc95ba392058926
Umer Farooqui <umfaruki@gmail.com>
2022-07-27 15:44:13 +0100

    Added DbContext common interface and implementation of Dbcontext and added customers

---
+++ src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+  
+
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
---
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using Application.Common.Interfaces;
+using Domain.Entities.Customers;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public DbSet<Customer> Customers => Set<Customer>();
+
+    public override  async Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        return result;
+    }
+}
