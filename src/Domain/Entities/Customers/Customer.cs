20035e13f596a26a79d6a551810257ffc700c45c
Umer Farooqui <umfaruki@gmail.com>
2022-07-27 15:45:30 +0100

    Initialized Instance properties to avoid warnings and also initialized DateJoined to current UTC date

--- src/Domain/Entities/Customers/Customer.cs
+++ src/Domain/Entities/Customers/Customer.cs
@@ -5,12 +5,16 @@
     /// </summary>
     public class Customer
     {
+        public Customer()
+        {
+            DateJoined = DateTime.UtcNow;
+        }
         public int Id { get; set; }
-        public string CustomerNumber { get; set; }
+        public string CustomerNumber { get; set; } = "";
         public DateTime DateJoined { get; set; }
-        public string Firstname { get; set; }
-        public string Surname { get; set; }
-        public string Email { get; set; }
-        public string Telephone { get; set; }
+        public string Firstname { get; set; } = "";
+        public string Surname { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Telephone { get; set; } = "";
     }
 }
