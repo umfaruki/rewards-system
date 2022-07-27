d3e0fc6ab187327cb28fb4ff1a2bea098e0cc528
Umer Farooqui <umfaruki@gmail.com>
2022-07-27 18:39:12 +0500

    Created Entity for Customer

---
+++ src/Domain/Entities/Customers/Customer.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Entities.Customers
+{
+    /// <summary>
+    /// This is Customer Entity
+    /// </summary>
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string CustomerNumber { get; set; }
+        public DateTime DateJoined { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Telephone { get; set; }
+    }
+}
