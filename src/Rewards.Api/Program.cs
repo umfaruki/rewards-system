f687d3b82c25842bc9fc17db5f0e017edc6f4a5c
Umer Farooqui <umfaruki@gmail.com>
2022-07-27 17:34:06 +0100

    Adding database migrations and seeding capabilities

--- src/Rewards.Api/Program.cs
+++ src/Rewards.Api/Program.cs
@@ -1,3 +1,5 @@
+using Infrastructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -14,6 +16,17 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+
+    // Initialise and seed database
+    using (var scope = app.Services.CreateScope())
+    {
+        var initialiser = scope.ServiceProvider.GetRequiredService<ApplicationDbContextInitialiser>();
+        await initialiser.InitialiseAsync();
+        await initialiser.SeedAsync();
+    }
+
+
 }
 
 app.UseHttpsRedirection();
