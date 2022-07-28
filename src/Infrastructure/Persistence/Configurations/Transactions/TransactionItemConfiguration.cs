a2c3f535c72c472d12cbbd3e3f2aa0e68676e7ae
Umer Farooqui <umfaruki@gmail.com>
2022-07-28 13:25:54 +0100

    Created TransactionItems Configurations and handled relationships

---
+++ src/Infrastructure/Persistence/Configurations/Transactions/TransactionItemConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities.Customers;
+using Domain.Entities.Transactions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.Transactions;
+
+public class TransactionItemConfiguration : IEntityTypeConfiguration<TransactionItem>
+{
+    public void Configure(EntityTypeBuilder<TransactionItem> builder)
+    {
+        builder.Property(t => t.ItemDescription)
+            .IsRequired().HasMaxLength(100);
+
+        builder.Property(t => t.Price)
+           .IsRequired();
+             
+        builder.Property(t => t.Quantity)            
+            .IsRequired();
+            
+        builder.Property(t => t.TransactionId)            
+            .IsRequired();
+
+        builder.HasOne<Transaction>(t => t.Transaction);
+    }
+}
---
+++ src/Infrastructure/Persistence/Migrations/20220728122456_Created-TransactionItem-Configurations.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class CreatedTransactionItemConfigurations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ItemDescription",
+                table: "TransactionItems",
+                type: "character varying(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ItemDescription",
+                table: "TransactionItems",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
