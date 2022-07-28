184a0434b1027e8b81f86022fe6000d7b78fd63e
Umer Farooqui <umfaruki@gmail.com>
2022-07-28 17:04:54 +0500

    Added Transaction Entity Configurations and updated the Migraiton

---
+++ src/Infrastructure/Persistence/Configurations/Transactions/TransactionConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Entities.Customers;
+using Domain.Entities.Transactions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.Transactions;
+
+public class TransactionConfiguration : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        builder.Property(t => t.DateCreated)
+            .IsRequired();
+
+        builder.Property(t => t.MinSpentAmount)
+           .IsRequired();
+             
+        builder.Property(t => t.UpperRangeSpentAmount)            
+            .IsRequired();
+            
+        builder.Property(t => t.CustomerId)            
+            .IsRequired();
+
+        builder.HasOne<Customer>(t => t.Customer);
+    }
+}
