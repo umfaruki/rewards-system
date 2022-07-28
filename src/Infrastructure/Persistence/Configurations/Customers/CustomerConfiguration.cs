8c0d84ab643e9dfa973d266621c5a58dc0285fd9
Umer Farooqui <umfaruki@gmail.com>
2022-07-28 16:47:50 +0500

    Added Customer Entity Configurations and updated the Migraiton

---
+++ src/Infrastructure/Persistence/Configurations/Customers/CustomerConfiguration.cs
@@ -0,0 +1,38 @@
+﻿using Domain.Entities.Customers;
+using Domain.Entities.Transactions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations.Customers;
+
+public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+{
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.Property(t => t.CustomerNumber)
+            .HasMaxLength(5)
+            .IsRequired();
+
+        builder.Property(t => t.DateJoined)           
+           .IsRequired();
+
+        builder.Property(t => t.Email)
+          .HasMaxLength(150)
+          .IsRequired();
+
+        builder.Property(t => t.Firstname)
+            .HasMaxLength(50)
+            .IsRequired();
+
+        builder.Property(t => t.Surname)
+           .HasMaxLength(50)
+           .IsRequired();
+
+        builder.Property(t => t.Telephone)
+            .HasMaxLength(15)
+            .IsRequired();
+
+        builder.HasMany<Transaction>(t => t.Transactions).WithOne(t => t.Customer);       
+
+    }
+}
---
+++ src/Infrastructure/Persistence/Migrations/20220728114649_Created-Customer-Entity-Configuration.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    public partial class CreatedCustomerEntityConfiguration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Customers",
+                type: "character varying(15)",
+                maxLength: 15,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Customers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Firstname",
+                table: "Customers",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Customers",
+                type: "character varying(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerNumber",
+                table: "Customers",
+                type: "character varying(5)",
+                maxLength: 5,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telephone",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(15)",
+                oldMaxLength: 15);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Surname",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Firstname",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CustomerNumber",
+                table: "Customers",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5)",
+                oldMaxLength: 5);
+        }
+    }
+}
